4af98beb3f4aadd0d2d67ccce1052578fef92b1b
DumDumbIch <dumdumbich@gmail.com>
2024-01-05 17:00:41 +0300

    A Compose Lazy Staggered Grid Tutorial (Ch.38)

--- app/src/main/java/ru/dumdumbich/steward/ui/screen/demo/DemoScreen.kt
+++ app/src/main/java/ru/dumdumbich/steward/ui/screen/demo/DemoScreen.kt
@@ -1,43 +1,25 @@
 package ru.dumdumbich.steward.ui.screen.demo
 
-import android.widget.Toast
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.staggeredgrid.LazyHorizontalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
-import kotlinx.coroutines.launch
 import ru.dumdumbich.steward.ui.theme.StewardTheme
+import kotlin.random.Random
 
 /**
  * <h3>Steward</h3>
@@ -48,118 +30,55 @@ import ru.dumdumbich.steward.ui.theme.StewardTheme
  * @date 2024-01-01 12:57
  **/
 
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun DemoScreen(itemArray: Array<out String>) {
-    val listState = rememberLazyListState()
-    val coroutineScope = rememberCoroutineScope()
-    val displayButton = listState.firstVisibleItemIndex > 5
 
-    val groupedItems = itemArray.groupBy { it.substringBefore(' ') }
-
-    val context = LocalContext.current
-
-    val onListItemClick = { text: String ->
-        Toast.makeText(
-            context,
-            text,
-            Toast.LENGTH_SHORT
-        ).show()
-    }
-
-    Box {
-        LazyColumn(
-            state = listState,
-            contentPadding = PaddingValues(bottom = 50.dp)
-        ) {
-            groupedItems.forEach { (manufacturer, models) ->
-
-                stickyHeader {
-                    Text(
-                        text = manufacturer,
-                        color = Color.White,
-                        modifier = Modifier
-                            .background(Color.Gray)
-                            .padding(5.dp)
-                            .fillMaxWidth()
-                    )
-                }
-
-                items(models) { model ->
-                    MyListItem(item = model, onItemClick = onListItemClick)
-                }
-            }
-        }
-
-        AnimatedVisibility(
-            visible = displayButton,
-            Modifier.align(Alignment.BottomCenter)
-        ) {
-            OutlinedButton(
-                onClick = {
-                    coroutineScope.launch {
-                        listState.scrollToItem(0)
-                    }
-                },
-                border = BorderStroke(1.dp, Color.Gray),
-                shape = RoundedCornerShape(50),
-                colors = ButtonDefaults.outlinedButtonColors(
-                    contentColor = Color.DarkGray
-                ),
-                modifier = Modifier.padding(5.dp)
-            ) {
-                Text(text = "Top")
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalCoilApi::class)
-@Composable
-fun ImageLoader(item: String) {
-
-    val url =
-        "https://www.ebookfrenzy.com/book_examples/car_logos/" + item.substringBefore(" ") + "_logo.png"
-
-    Image(
-        painter = rememberImagePainter(url),
-        contentDescription = "car image",
-        contentScale = ContentScale.Fit,
-        modifier = Modifier.size(75.dp)
-    )
-}
+data class BoxProperties(
+    val color: Color,
+    val width: Dp
+)
 
 @Composable
-fun MyListItem(item: String, onItemClick: (String) -> Unit) {
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.background
-        ),
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-            .clickable { onItemClick(item) },
-        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
-    )
-    {
+fun DemoScreen() {
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            ImageLoader(item)
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = item,
-                style = MaterialTheme.typography.headlineSmall,
-                modifier = Modifier.padding(8.dp)
+    val items = (1..50).map {
+        BoxProperties(
+            width = Random.nextInt(50, 200).dp,
+            color = Color(
+                Random.nextInt(255),
+                Random.nextInt(255),
+                Random.nextInt(255),
+                255
             )
+        )
+    }
+
+    LazyHorizontalStaggeredGrid(
+        rows = StaggeredGridCells.Fixed(3),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(8.dp),
+        horizontalItemSpacing = 8.dp,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(items) { values ->
+            GridItem(properties = values)
         }
     }
+}
+
+@Composable
+fun GridItem(properties: BoxProperties) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .width(properties.width)
+            .clip(RoundedCornerShape(10.dp))
+            .background(properties.color)
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DemoScreenPreview() {
-    val itemArray: Array<String> = arrayOf("Cadillac Eldorado", "Ford Fairlane", "Plymouth Fury")
     StewardTheme {
-        DemoScreen(itemArray = itemArray)
+        DemoScreen()
     }
 }
