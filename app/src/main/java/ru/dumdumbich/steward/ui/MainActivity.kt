c5b7e6e4d4b29c0cc89c0089cb8e5968720d5504
DumDumbIch <dumdumbich@gmail.com>
2024-01-05 11:55:07 +0300

    A Compose Lazy List Tutorial (Ch.36)

--- app/src/main/java/ru/dumdumbich/steward/ui/MainActivity.kt
+++ app/src/main/java/ru/dumdumbich/steward/ui/MainActivity.kt
@@ -7,11 +7,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import ru.dumdumbich.steward.R
 import ru.dumdumbich.steward.ui.screen.demo.DemoScreen
 import ru.dumdumbich.steward.ui.theme.StewardTheme
 
 class MainActivity : ComponentActivity() {
+
+    //private var itemArray: Array<String>? = null
+    private lateinit var itemArray: Array<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        itemArray = resources.getStringArray(R.array.car_array)
         super.onCreate(savedInstanceState)
         setContent {
             StewardTheme {
@@ -19,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DemoScreen()
+                    DemoScreen(itemArray = itemArray as Array<out String>)
                 }
             }
         }
--- app/src/main/java/ru/dumdumbich/steward/ui/screen/demo/DemoScreen.kt
+++ app/src/main/java/ru/dumdumbich/steward/ui/screen/demo/DemoScreen.kt
@@ -1,20 +1,29 @@
 package ru.dumdumbich.steward.ui.screen.demo
 
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Column
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import ru.dumdumbich.steward.ui.theme.StewardTheme
 
 /**
@@ -27,78 +36,71 @@ import ru.dumdumbich.steward.ui.theme.StewardTheme
  **/
 
 @Composable
-fun DemoScreen() {
-    ColumnList()
-    //RowList()
+fun DemoScreen(itemArray: Array<out String>) {
+
+    val context = LocalContext.current
+
+    val onListItemClick = { text: String ->
+        Toast.makeText(
+            context,
+            text,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    LazyColumn {
+        items(itemArray) { model ->
+            MyListItem(item = model, onItemClick = onListItemClick)
+        }
+    }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun ImageLoader(item: String) {
+
+    val url =
+        "https://www.ebookfrenzy.com/book_examples/car_logos/" + item.substringBefore(" ") + "_logo.png"
+
+    Image(
+        painter = rememberImagePainter(url),
+        contentDescription = "car image",
+        contentScale = ContentScale.Fit,
+        modifier = Modifier.size(75.dp)
+    )
 }
 
 @Composable
-fun ColumnList() {
+fun MyListItem(item: String, onItemClick: (String) -> Unit) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable { onItemClick(item) },
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+    )
+    {
 
-    val scrollState = rememberScrollState()
-    val coroutineScope = rememberCoroutineScope()
-
-    Column {
-
-        Row {
-            Button(onClick = {
-                coroutineScope.launch {
-                    scrollState.animateScrollTo(0)
-                }
-            },
-                modifier = Modifier
-                    .weight(0.5f)
-                    .padding(2.dp)) {
-                Text("Top")
-            }
-
-            Button(onClick = {
-                coroutineScope.launch {
-                    scrollState.animateScrollTo(scrollState.maxValue)
-                }
-            },
-                modifier = Modifier
-                    .weight(0.5f)
-                    .padding(2.dp)) {
-                Text("End")
-            }
-        }
-
-        Column(
-            Modifier.verticalScroll(scrollState)
-        ) {
-            repeat(500) {
-                Text(
-                    "List Item $it",
-                    style = MaterialTheme.typography.headlineSmall,
-                    modifier = Modifier.padding(5.dp)
-                )
-            }
-        }
-
-    }
-
-}
-
-@Composable
-fun RowList() {
-
-    val scrollState = rememberScrollState()
-
-    Row(Modifier.horizontalScroll(scrollState)) {
-        repeat(50) {
-            Text(" $it ",
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.padding(5.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            ImageLoader(item)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = item,
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(8.dp)
+            )
         }
     }
-
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DemoScreenPreview() {
+    val itemArray: Array<String> = arrayOf("Cadillac Eldorado", "Ford Fairlane", "Plymouth Fury")
     StewardTheme {
-        DemoScreen()
+        DemoScreen(itemArray = itemArray)
     }
 }
