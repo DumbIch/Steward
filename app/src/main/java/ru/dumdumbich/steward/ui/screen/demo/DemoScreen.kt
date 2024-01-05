d64b5dafa9050d4c8b7c18e5dfc1684f48489615
DumDumbIch <dumdumbich@gmail.com>
2024-01-05 12:54:17 +0300

    Lazy List Sticky Headers and Scroll Detection (Ch.37)

--- app/src/main/java/ru/dumdumbich/steward/ui/screen/demo/DemoScreen.kt
+++ app/src/main/java/ru/dumdumbich/steward/ui/screen/demo/DemoScreen.kt
@@ -1,8 +1,14 @@
 package ru.dumdumbich.steward.ui.screen.demo
 
 import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,19 +17,26 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
+import kotlinx.coroutines.launch
 import ru.dumdumbich.steward.ui.theme.StewardTheme
 
 /**
@@ -35,8 +48,14 @@ import ru.dumdumbich.steward.ui.theme.StewardTheme
  * @date 2024-01-01 12:57
  **/
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DemoScreen(itemArray: Array<out String>) {
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val displayButton = listState.firstVisibleItemIndex > 5
+
+    val groupedItems = itemArray.groupBy { it.substringBefore(' ') }
 
     val context = LocalContext.current
 
@@ -48,9 +67,49 @@ fun DemoScreen(itemArray: Array<out String>) {
         ).show()
     }
 
-    LazyColumn {
-        items(itemArray) { model ->
-            MyListItem(item = model, onItemClick = onListItemClick)
+    Box {
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 50.dp)
+        ) {
+            groupedItems.forEach { (manufacturer, models) ->
+
+                stickyHeader {
+                    Text(
+                        text = manufacturer,
+                        color = Color.White,
+                        modifier = Modifier
+                            .background(Color.Gray)
+                            .padding(5.dp)
+                            .fillMaxWidth()
+                    )
+                }
+
+                items(models) { model ->
+                    MyListItem(item = model, onItemClick = onListItemClick)
+                }
+            }
+        }
+
+        AnimatedVisibility(
+            visible = displayButton,
+            Modifier.align(Alignment.BottomCenter)
+        ) {
+            OutlinedButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.scrollToItem(0)
+                    }
+                },
+                border = BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.DarkGray
+                ),
+                modifier = Modifier.padding(5.dp)
+            ) {
+                Text(text = "Top")
+            }
         }
     }
 }
